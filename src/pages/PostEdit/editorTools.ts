1541825ac0a9a845e997a03216bd5d438b4b9cc4
Tiago Ferreira <sikozonbatata@gmail.com>
2020-03-21 12:11:56 +0000

    chore(parser): remove parser code

--- src/pages/PostEdit/editorTools.ts
+++ src/pages/PostEdit/editorTools.ts
@@ -46,39 +46,6 @@ export const getRandomQuote = () => {
   return QUOTES[Math.floor(Math.random() * QUOTES.length)]
 }
 
-export const convertCleanDataToHTML = (data: any) => {
-  var html = ''
-  data.blocks.forEach((block: any) => {
-    switch (block.type) {
-      case 'header':
-        html += `<h${block.data.level}>${block.data.text}</h${block.data.level}>`
-        break
-      case 'paragraph':
-        html += `<p>${block.data.text}</p>`
-        break
-      case 'delimiter':
-        html += '<hr />'
-        break
-      case 'image':
-        html += `<img class="img-fluid" src="${block.data.file.url}" title="${block.data.caption}" /><br /><em>${block.data.caption}</em>`
-        break
-      case 'list':
-        html += '<ul>'
-        block.data.items.forEach((li: string) => {
-          html += `<li>${li}</li>`
-        })
-        html += '</ul>'
-        break
-      default:
-        console.log('Unknown block type', block.type)
-        console.log(block)
-        break
-    }
-  })
-
-  return html
-}
-
 export const INITIAL_EDITOR_DATA = {
   time: 1556098174501,
   blocks: [
